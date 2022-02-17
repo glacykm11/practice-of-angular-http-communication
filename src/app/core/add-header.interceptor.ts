088c5ceeb4dd88bce47a83c2d51cb3b5ee78565a
Glacy <glacy.kelly3@gmail.com>
2022-02-17 20:08:05 -0300

    configuring log response interceptor

--- src/app/core/add-header.interceptor.ts
+++ src/app/core/add-header.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from "@angular/c
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class AddHeaderInterceptor implements HttpInterceptor{
+export class AddHeaderInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log(`AddHeaderInterceptor - ${req.url}`);
